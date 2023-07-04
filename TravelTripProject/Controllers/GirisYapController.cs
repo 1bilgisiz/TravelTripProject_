a85c2451d472555605311e4b9ed0dfceefd30043
1bilgisiz <bilgicsaniye66@gmail.com>
2023-07-04 21:47:37 +0300

    LogOut Apply

--- TravelTripProject/Controllers/GirisYapController.cs
+++ TravelTripProject/Controllers/GirisYapController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
@@ -40,7 +41,12 @@ namespace TravelTripProject.Controllers
             }
         
         }
+         public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
 
+            return RedirectToAction("Login", "GirisYap");
+        }
 
 
     }
